4e039d14ab5f5a03d88cbf4d8a57773fb524984d
zakafir <zak.afir@gmail.com>
2017-10-20 16:45:19 +0100

    Contract SQLite (updating)

--- app/src/main/java/com/st00/afir/guestlist/data/WaitlistContract.java
+++ app/src/main/java/com/st00/afir/guestlist/data/WaitlistContract.java
@@ -5,12 +5,12 @@ import android.provider.BaseColumns;
 public class WaitlistContract {
 
 
-    class WaitlistEntry implements BaseColumns {
+    public class WaitlistEntry implements BaseColumns {
 
-        static final String TABLE_NAME = "waitlist";
-        static final String COLUMN_GUEST_NAME = "guestName";
-        static final String COLUMN_PARTY_SIZE = "partySize";
-        static final String COLUMN_TIMESTAMP = "timestamp";
+        public static final String TABLE_NAME = "waitlist";
+        public static final String COLUMN_GUEST_NAME = "guestName";
+        public static final String COLUMN_PARTY_SIZE = "partySize";
+        public static final String COLUMN_TIMESTAMP = "timestamp";
 
     }
 }
